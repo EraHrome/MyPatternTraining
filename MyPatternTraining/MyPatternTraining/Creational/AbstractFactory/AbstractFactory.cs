2261d3cce2a3693a320e01e701a549695e4b1c0c
Nikita <polkovnikovnikita99@yandex.ru>
2023-11-08 22:26:15 +0300

    add abstract factory

---
+++ MyPatternTraining/MyPatternTraining/Creational/AbstractFactory/AbstractFactory.cs
@@ -0,0 +1,41 @@
+﻿using MyPatternTraining.TestClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPatternTraining.Creational.AbstractFactory
+{
+    internal abstract class AbstractFactory
+    {
+        internal abstract TestClass0 CreateTestClass0();
+        internal abstract TestClass1 CreateTestClass1();
+    }
+
+    internal class AbstractFactory0 : AbstractFactory
+    {
+        internal override TestClass0 CreateTestClass0()
+        {
+            return new TestClass0();
+        }
+
+        internal override TestClass1 CreateTestClass1()
+        {
+            return new TestClass1("from " + nameof(AbstractFactory0));
+        }
+    }
+
+    internal class AbstractFactory1 : AbstractFactory
+    {
+        internal override TestClass0 CreateTestClass0()
+        {
+            return new TestClass0();
+        }
+
+        internal override TestClass1 CreateTestClass1()
+        {
+            return new TestClass1("from " + nameof(AbstractFactory1));
+        }
+    }
+}
